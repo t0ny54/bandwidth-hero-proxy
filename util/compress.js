8843898ccc5cae0e20d4f36332ca26ff88061684
t0ny54 <antonyitunamaku@outlook.com>
2024-04-01 21:29:14 +0700

    Add files via upload

--- util/compress.js
+++ util/compress.js
@@ -1,34 +1,49 @@
-const sharp = require("sharp");
+"use strict";
+/*
+ * compress.js
+ * A module that compress a image.
+ * compress(httpRequest, httpResponse, ReadableStream);
+ */
+const sharp = require('sharp')
+const redirect = require('./redirect')
 
-function compress(input, webp, grayscale, quality, originSize) {
-	const format = webp ? "webp" : "jpeg";
+const sharpStream = _ => sharp({ animated: !process.env.NO_ANIMATE, unlimited: true });
 
-	return sharp(input)
-		.grayscale(grayscale)
-		.toFormat(format, {
-			quality: quality,
-			progressive: true,
-			optimizeScans: true
-		})
-		.toBuffer({resolveWithObject: true})
-		.then(({data: output,info}) => {	// this way we can also get the info about output image, like height, width
-		// .toBuffer()
-		// .then( output => {
-			return {
-				err: null,
-				headers: {
-					"content-type": `image/${format}`,
-					"content-length": info.size,
-					"x-original-size": originSize,
-					"x-bytes-saved": originSize - info.size,
-				},
-				output: output
-			};
-		}).catch(err => {
-			return {
-				err: err
-			};
-		});
+function compress(req, res, input) {
+  const format = req.params.webp ? 'webp' : 'jpeg'
+
+  /*
+   * Determine the uncompressed image size when there's no content-length header.
+   */
+
+  /*
+   * input.pipe => sharp (The compressor) => Send to httpResponse
+   * The following headers:
+   * |  Header Name  |            Description            |           Value            |
+   * |---------------|-----------------------------------|----------------------------|
+   * |x-original-size|Original photo size                |OriginSize                  |
+   * |x-bytes-saved  |Saved bandwidth from original photo|OriginSize - Compressed Size|
+   */
+  input.body.pipe(sharpStream()
+    .grayscale(req.params.grayscale)
+    .toFormat(format, {
+      quality: req.params.quality,
+      progressive: true,
+      optimizeScans: true
+    })
+    .toBuffer((err, output, info) => _sendResponse(err, output, info, format, req, res)))
+}
+
+function _sendResponse(err, output, info, format, req, res) {
+  if (err || !info) return redirect(req, res);
+
+  res.setHeader('content-type', 'image/' + format);
+  res.setHeader('content-length', info.size);
+  res.setHeader('x-original-size', req.params.originSize);
+  res.setHeader('x-bytes-saved', req.params.originSize - info.size);
+  res.status(200);
+  res.write(output);
+  res.end();
 }
 
 module.exports = compress;
--- util/shouldCompress.js
+++ util/shouldCompress.js
@@ -1,18 +1,23 @@
-const MIN_COMPRESS_LENGTH = 1024;
-const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
+"use strict";
+const MIN_COMPRESS_LENGTH = 1024
+const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100
 
-function shouldCompress(originType, originSize, webp) {
-	if ( !originType.startsWith("image") || originSize === 0) return false;
-	if (webp && originSize < MIN_COMPRESS_LENGTH) return false;
-	if (	// if png or gif image, and size is less than compression limit (also shouldn't be webp)
-		!webp &&
-		(originType.endsWith("png") || originType.endsWith("gif")) &&
-		originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
-	) {
-		return false;
-	}
+function shouldCompress(req) {
+  const { originType, originSize, webp } = req.params
 
-	return true;
+  if (!originType.startsWith('image')) return false;
+  if (originSize === 0) return false;
+  if (req.headers.range) return false;
+  if (webp && originSize < MIN_COMPRESS_LENGTH) return false;
+  if (
+    !webp &&
+    (originType.endsWith('png') || originType.endsWith('gif')) &&
+    originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
+  ) {
+    return false;
+  }
+
+  return true;
 }
 
-module.exports = shouldCompress;
+module.exports = shouldCompress
